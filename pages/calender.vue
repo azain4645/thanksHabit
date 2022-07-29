828d47834a92235cf0ce4897bc9ddb29699a5bfa
masamiti <azain4645@yahoo.co.jpgit push origin sec-02_componentsSlot>
2022-07-29 23:36:24 +0900

    カレンダーの配列作成途中

--- pages/calender.vue
+++ pages/calender.vue
@@ -1,7 +1,44 @@
-<script setup lang="ts">
+<script setup>
+import { DateTime } from "luxon";
 
+const now = DateTime.local();
+const startDate = computed(() => {
+  const newDt = now.startOf("month")
+  const youbiNum = newDt.weekday
+  return newDt.minus({
+    days: youbiNum
+  })
+});
+const endDate = computed(() => {
+  const endDt = now.endOf("month")
+  let youbiNum = endDt.weekday
+  youbiNum == 7 ? youbiNum = 0 : youbiNum = youbiNum
+  
+  return endDt.plus({
+    days: 6 - youbiNum
+  })
+});
+const calender = computed(() => {
+  const weekNumber = Math.ceil(endDate.value.diff(startDate.value, 'days').days / 7);
+  let date = startDate.value
+
+  let calendars = [];
+  for (let week = 0; week < weekNumber; week++) {
+    let weekRow = [];
+    for (let day = 0; day < 7; day++) {
+      weekRow.push({
+        date: date.day,
+      });
+      date.plus({days: 1})
+    }
+    calendars.push(weekRow)
+  }
+  return calendars;
+});
 </script>
 
 <template>
-aaa
+  {{ startDate }}
+  {{ endDate }}
+  {{ calender }}
 </template>
