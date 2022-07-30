34610fa157bb0d5683ec6325251d22d8843ad1c7
masamiti <azain4645@yahoo.co.jpgit push origin sec-02_componentsSlot>
2022-07-30 21:46:10 +0900

    デザイン途中

--- pages/calender.vue
+++ pages/calender.vue
@@ -2,6 +2,9 @@
 import { DateTime } from "luxon";
 
 const now = ref(DateTime.local());
+const displayDate = ref(now.value.toFormat('yyyy年M月'))
+const currentMonth = now.value.toFormat('YYYY-MM')
+const youbi = ["日", "月", "火", "水", "木", "金", "土"]
 
 const startDate = computed(() => {
   const newDt = now.value.startOf("month")
@@ -31,12 +34,12 @@ const calenders = computed(() => {
     for (let day = 0; day < 7; day++) {
       weekRow.push({
         date: date.day,
+        month: date.toFormat('YYYY-MM'),
       });
       date = date.plus({days: 1})
     }
     calendars.push(weekRow)
   }
-  console.log(calendars)
   return calendars;
 });
 
@@ -45,24 +48,39 @@ const nextMonth = () => now.value = now.value.plus({ months: 1})
 </script>
 
 <template>
-  <h2>カレンダー {{ now }}</h2>
+  <h2>カレンダー 
+    {{ displayDate }}
+  </h2>
   <button @click="prevMonth">前の月</button>
   <button @click="nextMonth">次の月</button>
 
-  <div style="margin:auto;max-width:900px;border-top:1px solid gray;">
+  <div 
+    style="margin:auto;max-width:900px;"
+    class="border-t border-gray-300 border-solid"
+  >
+    <div class="flex text-center border-l border-gray-300 border-solid">
+      <div 
+        v-for="n in 7" 
+        :key="n"
+        style="flex:1"
+        class="border-r border-gray-300 border-solid"
+      >
+        {{ youbi[n-1] }}
+      </div>
+    </div>
     <div 
       v-for="(week,index) in calenders" 
       :key="index" 
-      style="display:flex;border-left:1px solid gray"
+      class="flex border-l border-gray-300 border-solid"
     >
+    
       <div 
         v-for="(day, index) in week" 
         :key="index"
-        style="
-            flex:1;min-height:100px;
-            border-right:1px solid gray;
-            border-bottom:1px solid gray
-          "
+        style="flex:1;min-height:100px;"
+        :class="`${ 
+          currentMonth != day.month ? 'bg-gray-100' : ''
+        } border-r border-b border-gray-300 border-solid text-center`"
       >
         {{ day.date }}
       </div>
